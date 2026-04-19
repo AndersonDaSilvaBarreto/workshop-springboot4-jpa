ca2706f11fa642b6c420eb7c10cc32bc2874348f
Anderson <andersondasilvabarretoasb@gmail.com>
2026-04-18 23:02:10 -0300

    Order, Instant, ISO 8601 - Part 2

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -1,6 +1,8 @@
 package com.educandoweb.course.config;
 
+import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
 import org.jspecify.annotations.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 @Configuration
@@ -15,6 +18,8 @@ import java.util.Arrays;
 public class TestConfig implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private OrderRepository orderRepository;
 
     @Override
     public void run(String @NonNull ... args) {
@@ -29,5 +34,15 @@ public class TestConfig implements CommandLineRunner {
         u2.setPhone("977777777");
         u2.setPassword("123456");
         userRepository.saveAll(Arrays.asList(u1, u2));
+        Order o1 = new Order();
+        o1.setMoment(Instant.parse("2019-06-20T19:53:07Z"));
+        o1.setClient(u1);
+        Order o2 = new Order();
+        o2.setMoment(Instant.parse("2019-07-21T03:42:10Z"));
+        o2.setClient(u2);
+        Order o3 = new Order();
+        o3.setMoment(Instant.parse("2019-07-22T15:21:22Z"));
+        o3.setClient(u1);
+        orderRepository.saveAll(Arrays.asList(o1,o2,o3));
     }
 }
--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,16 +1,17 @@
 package com.educandoweb.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
@@ -18,6 +19,7 @@ public class User implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
     private String  name;
     private String email;
@@ -25,19 +27,9 @@ public class User implements Serializable {
     private String password;
 
     @Setter(AccessLevel.NONE)
+    @JsonIgnore
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
 
 
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
 }
